3fe02d021b439028970c24f82d2143f78477a7b7
krodriguez804 <krodriguez804@gmail.com>
2026-03-11 20:04:34 -0700

    5

--- lab09-main.cpp
+++ lab09-main.cpp
@@ -8,97 +8,78 @@ using namespace std;
 
 int main() {
 
-    ArrayStack<int> i(3);
-    
-    cout << "Max size: " << i.getMaxSize() << endl;
+  ArrayStack<int> a(10);
+  ArrayStack<int> b(a);
+  ArrayStack<int> c(10);
 
-    i.push(742);
-    i.push(461);
-    i.push(726);
+  a.push(0);
+  a.push(1);
+  a.push(2);
+  a.push(3);
+  a.push(4);
+  a.push(5);
+  a.push(6);
+  a.push(7);
+  a.push(8);
+  a.push(9);
 
-    cout << "Length: " << i.getLength() << endl;
-    cout << "Empty? " << i.isEmpty() << endl;
-    cout << "Full? " << i.isFull() << endl;
+  cout << a << endl;
 
-    cout << "Peek: " << i.peek() << endl;
-    
-    i.pop();
-    
-    cout << "Stack after pop: " << i << endl;
-    
-    i.clear();
-    
-    cout << "Stack after clear: " <<  i << endl;
+  if (a.isFull()) {
+    cout << "Stack A is full!" << endl;
+  }
 
-    ArrayStack<string> s(3);
-    
-    cout << "Max size: " << s.getMaxSize() << endl;
+  cout << "Stack A length: " << a.getLength() << endl;
+  cout << "Stack B length: " << b.getLength() << endl;
 
-    s.push("hello");
-    s.push("hi");
-    s.push("hey");
+  try {
+    a.push(12);
+  }
+  catch (string e) {
+    cout << e << endl;
+  }
 
-    cout << "Length: " << s.getLength() << endl;
-    cout << "Empty? " << s.isEmpty() << endl;
-    cout << "Full? " << s.isFull() << endl;
+  b.pop();
+  cout << "Popping 9 from stack B" << endl;
 
-    cout << "Peek: " << s.peek() << endl;
+  b.pop();
+  cout << "Popping 8 from stack B" << endl;
     
-    s.pop();
+  b.pop();
+  cout << "Popping 7 from stack B" << endl;
     
-    cout << "Stack after pop: " << s << endl;
+  b.pop();
+  cout << "Popping 6 from stack B" << endl;
     
-    s.clear();
+  b.pop();
+  cout << "Popping 5 from stack B" << endl;
     
-    cout << "Stack after clear: " <<  s << endl;
+  cout << "Stack B length: " << b.getLength() << endl;
 
-    ArrayStack<double> d(3);
-    
-    cout << "Max size: " << d.getMaxSize() << endl;
+  b.clear();
 
-    d.push(742.21);
-    d.push(461.256);
-    d.push(726.781);
+  cout << "Stack B is now empty after being cleared" << endl;
 
-    cout << "Length: " << d.getLength() << endl;
-    cout << "Empty? " << d.isEmpty() << endl;
-    cout << "Full? " << d.isFull() << endl;
+  try {
+    b.pop();
+  }
+  catch (string e) {
+    cout << e << endl;
+  }
 
-    cout << "Peek: " << d.peek() << endl;
-    
-    d.pop();
-    
-    cout << "Stack after pop: " << d << endl;
-    
-    d.clear();
-    
-    cout << "Stack after clear: " << d << endl;
-
-    ArrayStack<char> c(3);
-    
-    cout << "Max size: " << c.getMaxSize() << endl;
-
-    c.push('a');
-    c.push('b');
-    c.push('c');
-
-    cout << "Length: " << c.getLength() << endl;
-    cout << "Empty? " << c.isEmpty() << endl;
-    cout << "Full? " << c.isFull() << endl;
-
-    cout << "Peek: " << c.peek() << endl;
-    
-    c.pop();
-    
-    cout << "Stack after pop: " << c << endl;
-    
-    c.clear();
-    
-    cout << "Stack after clear: " <<  c << endl;
+  try {
+    b.peek();
+  }
+  catch (string e) {
+    cout << e << endl;
+  }
+ 
+  cout << "Stack A length: " << a.getLength() << endl;
+  cout << "Stack C length: " << c.getLength() << endl;
 
     return 0;
 }
 
 /*
-  //  i.rotate(LEFT);
+    //  i.rotate(LEFT);
 */
